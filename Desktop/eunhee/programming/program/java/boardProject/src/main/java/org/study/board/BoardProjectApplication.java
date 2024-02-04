f6767b540ff491d27c33158c40bd7a7ad15987b2
eunhee <eunheeJ@tmax.co.kr>
2024-02-04 14:23:08 +0900

    [chore] 필요없는 파일 삭제, 주석처리

---
+++ Desktop/eunhee/programming/program/java/boardProject/src/main/java/org/study/board/BoardProjectApplication.java
@@ -0,0 +1,13 @@
+package org.study.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectApplication.class, args);
+	}
+
+}
