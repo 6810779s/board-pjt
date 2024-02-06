2e4cd81d1cfdba5dabab99210f4c97b23c341cc2
eunhee <6810779s@naver.com>
2024-02-06 22:39:27 +0900

    [feat] 파일 잘못올라간것들 모두 삭제 후 다시 업로드

---
+++ src/main/java/org/study/board/BoardProjectApplication.java
@@ -0,0 +1,15 @@
+package org.study.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class BoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/study/board/domain/ArticleComment.java
@@ -0,0 +1,46 @@
+package org.study.board.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {@Index(columnList = "content"), @Index(columnList = "createdAt"), @Index(columnList = "createdBy")})
+@Entity
+public class ArticleComment extends AuditingFields{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter @ManyToOne(optional = false) private Article article;
+    @Setter @Column(nullable = false, length = 500) private String content;
+
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
